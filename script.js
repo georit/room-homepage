6cd322a40e2f58047cd71cc7a88cbe7ff59d7b44
George Mapaya <mambostudioz@gmail.com>
2020-11-25 14:19:56 -0600

    Add media queries & functionality

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
-/* *****variables***** */
+/* segment *****variables***** */
+const dataObjects = [
+  {
+    name: "discover",
+    bgMobile: "url('./images/mobile-image-hero-1.jpg')",
+    bgDesktop: "url('./images/desktop-image-hero-1.jpg')",
+    heading: "Discover innovative ways to decorate",
+    info:
+      "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+  },
+  {
+    name: "location",
+    bgMobile: "url('./images/mobile-image-hero-2.jpg')",
+    bgDesktop: "url('./images/desktop-image-hero-2.jpg')",
+    heading: "We are available all across the globe",
+    info:
+      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest to you using our store locator. Any questions? Don't hesitate to contact us today.",
+  },
+  {
+    name: "quality",
+    bgMobile: "url('./images/mobile-image-hero-3.jpg')",
+    bgDesktop: "url('./images/desktop-image-hero-3.jpg')",
+    heading: "Manufactured with the best materials",
+    info:
+      "Our modern furniture store provides a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+  },
+];
+
 const btnShowMobileNav = document.getElementById("mobile-nav-open");
 const mobileNavContainer = document.querySelector(".nav-links-container");
 const mobileNav = document.getElementById("mobile-nav");
 const btnCloseMobileNav = document.getElementById("mobile-nav-close");
 const bodyEl = document.querySelector("body");
 
-/* *****functions***** */
+const heroEl = document.querySelector(".hero");
+const mainHeading = document.querySelector(".main-heading");
+const detailsInfo = document.querySelector(".details-info");
+
+// previous & next control buttons
+const btnPrev = document.querySelector(".fa-chevron-left");
+const btnNext = document.querySelector(".fa-chevron-right");
+
+let infoIndex = 0;
+
+/* *****onload***** */
+displayInfo(infoIndex);
+
+/* segment *****functions***** */
+function displayInfo(n) {
+  let width = onResize();
+  if (window.innerWidth > 430 || width > 430) {
+    heroEl.style.backgroundImage = dataObjects[n].bgDesktop;
+  } else {
+    heroEl.style.backgroundImage = dataObjects[n].bgMobile;
+  }
+
+  mainHeading.innerHTML = dataObjects[n].heading;
+  detailsInfo.innerHTML = dataObjects[n].info;
+}
+
 function showMobileNav() {
   mobileNav.classList.remove("hidden");
   bodyEl.classList.add("scroll");
@@ -16,7 +68,29 @@ function hideMobileNav() {
   bodyEl.classList.remove("scroll");
 }
 
-/* *****event listeners***** */
+function onResize() {
+  let width;
+  if (typeof window.innerWidth == "number") {
+    width = window.innerWidth;
+  } else {
+    if (document.documentElement && document.documentElement.clientWidth) {
+      width = document.documentElement.clientWidth;
+    } else {
+      if (document.body && document.body.clientWidth) {
+        width = document.body.clientWidth;
+      }
+    }
+  }
+  return width;
+}
+
+/* segment *****event listeners***** */
+// when window is resized
+window.addEventListener("resize", () => {
+  onResize();
+  displayInfo(infoIndex);
+});
+
 // show mobile navigation
 btnShowMobileNav.addEventListener("click", showMobileNav);
 
@@ -25,20 +99,6 @@ btnCloseMobileNav.addEventListener("click", hideMobileNav);
 
 // go to navigation link
 mobileNavContainer.addEventListener("click", (e) => {
-  /*let navLink = e.target.id;
-
-  if (
-    navLink === "nav-link-1" ||
-    navLink === "nav-link-2" ||
-    navLink === "nav-link-3" ||
-    navLink === "nav-link-4"
-  ) {
-    console.log(navLink);
-    mobileNav.classList.add("hidden");
-  } else {
-    console.log("nothing to see here");
-  }*/
-
   let navLink = e.target;
 
   if (navLink.className === "nav-link-anchor about-anchor") {
@@ -51,4 +111,25 @@ mobileNavContainer.addEventListener("click", (e) => {
   if (navLink.className === "nav-link-anchor") {
     hideMobileNav();
   }
+});
+
+// controls
+btnPrev.addEventListener("click", () => {
+  infoIndex--;
+
+  if (infoIndex < 0) {
+    infoIndex = dataObjects.length - 1;
+  }
+
+  displayInfo(infoIndex);
+});
+
+btnNext.addEventListener("click", () => {
+  infoIndex++;
+
+  if (infoIndex > dataObjects.length - 1) {
+    infoIndex = 0;
+  }
+
+  displayInfo(infoIndex);
 });
